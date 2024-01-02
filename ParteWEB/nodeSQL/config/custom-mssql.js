9b6b268c8ce5c473bacbdb2980c3e9f99d63144a
Maria Luiza Sperancin Mancebo <50486625+malusperancin@users.noreply.github.com>
2024-01-02 13:35:20 -0300

    Update custom-mssql.js

--- ParteWEB/nodeSQL/config/custom-mssql.js
+++ ParteWEB/nodeSQL/config/custom-mssql.js
@@ -1,9 +1,5 @@
 var mssql = require('mssql');
 const config = {
-user: 'BD19173',
-password: '',
-server: 'regulus.cotuca.unicamp.br',
-database: 'BD19173'
 };
 mssql.connect(config)
 .then(conexao => global.conexao = conexao)
